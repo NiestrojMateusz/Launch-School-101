72fb35475046a361e59670581681078cb821a42b
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2016-11-10 12:40:53 +0000

    Add medium 1 exercices

--- lesson_3/Easy_3.rb
+++ lesson_3/Easy_3.rb
@@ -49,3 +49,11 @@ advice.slice!(0, advice.index('house'))
 statement = "The Flintstones Rock!"
 
 statement.scan('t').count
+
+#Question 6
+
+#Back in the stone age (before CSS) we used spaces to align things on the screen. If we had a 40 character wide table of Flintstone family members, how could we easily center that title above the table with spaces?
+
+title = "Flintstone Family Members"
+
+title.center(40)
